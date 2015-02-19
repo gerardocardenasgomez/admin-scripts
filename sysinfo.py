5b4e786dd92c46eba35f3008542d24a3a21181ba
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-02-19 06:53:47 +0000

    Add yum-based script that records pkg/sys info
    Next version should be able to handle apt-based systems

---
+++ sysinfo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import platform
+import rpm
+
+
+pkgFilter = []
+
+with open("packages.txt", "r") as file:
+    for line in file:
+        pkgString = line
+        pkgFilter.append(pkgString.replace("\n", ""))
+
+distName, distVersion, distID = platform.linux_distribution()
+sysType = platform.machine()
+sysName = platform.system()
+sysRelease = platform.release()
+
+db = rpm.TransactionSet()
+mdb = db.dbMatch()
+
+print "-" * 5, " * SYSINFO * ", "-" * 5
+
+print "{0} {1} -- Kernel {2}".format(sysName, sysType, sysRelease)
+print "{0} {1} {2}".format(distName, distVersion, distID)
+
+print "-" * 5, " * PACKAGES * ", "-" * 5
+
+for item in mdb:
+    pkgName = item['name']
+    pkgVersion = item['version']
+    pkgRelease = item['release']
+
+    if pkgName in pkgFilter:
+        continue
+    else:
+        print "{0} {1} ({2})".format(pkgName, pkgVersion, pkgRelease)
