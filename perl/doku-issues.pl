cc2c57d57c3fb5eb185dce35b5f8003cb4ffdd41
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-04-07 18:58:51 -0400

    Update doku-issues script to write to target file
    The doku-issues.pl script now takes one argument
        which will be the target file. And the rest of
        the files in the argument list will be searched
        for the "FIXME" string. The script copies the entire
        line once it encounters the string and
        adds it to an array.
    
    Then the target file is pruned after a "Fix Me Links" string
        and then the array of "FIXME" lines is added to the
        target file.

--- perl/doku-issues.pl
+++ perl/doku-issues.pl
@@ -1,10 +1,22 @@
 #!/usr/bin/env perl
 
+# FIXES array will contain all the issues that the script finds.
 @FIXES = ();
-$cats = "hi";
+
+# The first arg is the file to write to
+$target = $ARGV[0];
 
 foreach $file (@ARGV) {
+
+    # The file to write to should not be included in the search
+    # If it were included in the search, fixme issues would be duplicated
+    if ($file eq $target) {
+        next;
+    }
+
     open FILE, "<", "$file" or die $!;
+
+    # Search for the FIXME string
     while (<FILE>) {
         if ($_ =~ /FIXME.*\(/) {
             $file =~ s/.txt/$1/;
@@ -14,6 +26,26 @@ foreach $file (@ARGV) {
     }
 }
 
-foreach $line (@FIXES) {
-    print $line, "\n";
+open FILE, "<", $target or die $!;
+
+@LINES = <FILE>;
+close(FILE);
+
+open FILE, ">", $target or die $!;
+
+foreach my $LINE ( @LINES ) {
+    print FILE $LINE;
+
+    # If the line "Fix Me Links" is found in the file
+    # It means we have found the location where we want to start putting the links
+    # Erase everything after the "Fix Me Link" line and insert a new line
+    # Then write the @FIXES array to the end of the target file.
+    if ($LINE =~ /Fix Me Links/) {
+        print FILE "\n";
+        foreach $line (@FIXES) {
+            $insert = $line . "\n";
+            print FILE $insert;
+        }
+        exit(0);
+    }
 }
