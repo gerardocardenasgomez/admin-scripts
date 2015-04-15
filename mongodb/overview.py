49fb84c30b806cb860fa7aa88b05865c153cd0ce
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-04-15 22:17:14 +0000

    Overview now checks github api for recent commit
    overview.py file now checks github api for the most recent commit on my github account. For the future, it should check the most recent commit in the last 2 days but currently the app has a statis date set.
    
    Additionally, the blog and commit fetching methods were moved to a separate file.

--- mongodb/overview.py
+++ mongodb/overview.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 import re
-from flask import Flask, request
+from flask import Flask, request, render_template
 import pymongo
 import urllib2
 import xml.etree.ElementTree as ET
+import blogimport
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "welcome, hi"
+    blog_title = "Cars"
+    entry_title = "bears"
+    latest_post = "a bunch of text herehrhe"
+
+    output_text = { "blog_title": blog_title,
+                    "entry_title" : entry_title,
+                    "latest_post" : latest_post}
+
+    return render_template('latest_post.html',
+                    blog_title  = blog_title,
+                    entry_title = entry_title,
+                    latest_post = latest_post)
 
 @app.route('/user/<name>')
 def user(name):
@@ -39,40 +51,20 @@ def user(name):
 
 @app.route('/overview')
 def overview():
-    response = urllib2.urlopen('https://gerardobsd.com:443/rss/')
-    data = response.read()
-    
-    root = ET.fromstring(data)
-    
-    titles = []
-    for item in root.iter('title'):
-        titles.append(item)
+    blog_title, entry_title, latest_post = blogimport.recent_post('https://gerardobsd.com:443/rss/')
 
-    blog_title = titles[0].text
-    entry_title = titles[1].text
-
-    blog_desc = []
-
-    for item in root.iter('description'):
-        blog_desc.append(item)
-
-    latest_post = blog_desc[1].text
-
-    start_tag_pattern = re.compile(r'\<p\>')
-    end_tag_pattern = re.compile(r'\<\/p\>')
-
-    start_index = re.search(start_tag_pattern, latest_post).start()
-    start_index = start_index + 3
-
-    end_index = re.search(end_tag_pattern, latest_post).start()
-
-    latest_post = latest_post[start_index:end_index]
+    commit_msg, commit_date, commit_url = blogimport.recent_commit('https://api.github.com/repos/gerardocardenasgomez/admin-scripts/commits?since=2015-04-11T11:59:00Z')
 
     output_text = "<h1>" + blog_title + "</h1>"
     output_text = output_text + "<h2>" + entry_title + "</h2>"
     output_text = output_text + "<p>" + latest_post + "</p>"
 
+    output_text = output_text + "<h1>" + "Recent git Commit" + "</h1>"
+    output_text = output_text + "<p>" + commit_msg + "</p>"
+    output_text = output_text + "<p>" + commit_date + "</p>"
+    output_text = output_text + """<p> <a href="{0}" alt="Recent Commit">{1}</a></p>""".format(commit_url, commit_url)
+
     return output_text 
 
 if __name__ == '__main__': 
-    app.run(host='', port=8080, debug=True)
+    app.run(host='100.78.122.25', port=8080, debug=True)
