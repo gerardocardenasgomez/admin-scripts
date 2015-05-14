147c53873e4690916c82bcb0217dd758d8190811
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-05-13 21:09:40 -0400

    Script to dump wordlist into elasticsearch
    WARNING: this script is "dump" and might crash elasticsearch

---
+++ bash/es_mass.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import random
+import urllib2
+import json
+import sys
+
+domain = sys.argv[1]
+url = 'http://{0}:9200/lists/words/'.format(domain)
+print url
+sanity = 5
+
+with open('wordlist', 'r') as f:
+    for line in f:
+        word = line.rstrip('\n')
+        id_num = str(random.randrange(1, 1000000))
+        url = url + id_num
+        word_length = len(word)
+        data = json.dumps({"word" : word, "word_length" : word_length})
+        
+        req = urllib2.Request(url, data, {'Content-Type' : 'application/json'})
+        retval = urllib2.urlopen(req)
+        response = retval.read()
+
+        if sanity > 0:
+            print response
+            sanity -= 1
+
+        retval.close()
