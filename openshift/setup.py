220243c1bf22f2e719626068aaae8de1107a418d
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-06-23 12:35:23 -0400

    Update to display summary

---
+++ openshift/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='myOverview',
+      version='0.1',
+      description='App that provides an overview of my recent work.',
+      author='Gerardo Cardenas',
+      author_email='gerardo@gerardobsd.com',
+      url='gerardobsd.com',
+      install_requires=['pymongo'],
+     )
