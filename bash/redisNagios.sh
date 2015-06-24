85e977ab3077cdb618da79dea0fbda1cfef9665b
Gerardo Cardenas-Gomez <gerardo@gerardobsd.com>
2015-06-24 18:37:37 +0000

    Script that will be used in Nagios

--- bash/redisNagios.sh
+++ bash/redisNagios.sh
@@ -1,23 +1,88 @@
 #!/usr/bin/env bash
 
-hours_hard=12
-hours_soft=6
+redis_fetch () {
+    ip_addr=$1
+    query_type=$2
+    target=$3
 
-redis_server=$1
+    case $query_type in
+    *INFO*)
+        results=$(redis-cli -h $ip_addr INFO | grep "$target:" | awk -F: '{print $2}' | tr -d '\r') 
+        ;;
+    *CONFIG*)
+        results=$(redis-cli -h $ip_addr CONFIG GET $target | perl -ne 'print if $. == "2";')
+        ;;
+    esac
 
-results=$(redis-cli -h 127.0.0.1 INFO Persistence | grep rdb_last_save_time | awk -F: '{print $2}' | tr -d '\r')
-now=$(date +%s | tr -d '\r')
+    echo $results
+}
 
-difference=$((now - results))
-hours=$((difference / 3600))
+check_last_backup() {
+    hours_hard=12
+    hours_soft=6
 
-if [[ $hours -gt $hours_soft ]]; then
-    if [[ $hours -gt $hours_hard ]]; then
-        echo "CRITICAL: It has been $hours since the last save!"
-        exit 2
+    ip_addr=$1
+
+    results=$(redis_fetch $redis_ipaddr INFO rdb_last_save_time)
+    now=$(date +%s | tr -d '\r')
+
+    difference=$((now - results))
+    hours=$((difference / 3600))
+
+    if [[ $hours -gt $hours_soft ]]; then
+        if [[ $hours -gt $hours_hard ]]; then
+            echo "CRITICAL: It has been $hours since the last save!"
+            exit 2
+        fi
+    
+        echo "WARNING: It has been $hours since the last save!"
+            exit 1
+    else
+        echo "OK: It has been $hours since the last save."
+        exit 0
+    fi
+}
+
+perform_backup() {
+    ip_addr=$1
+    target_dir=$2
+
+    redis_dbname=$(redis_fetch $ip_addr CONFIG dbfilename)
+    redis_dir=$(redis_fetch $ip_addr CONFIG dir)
+
+    redis_output=$(redis-cli -h $ip_addr BGSAVE)
+    if [[ ! $redis_output =~ .*started.* ]]; then
+        echo $redis_output
+        exit 99
     fi
     
-    echo "WARNING: It has been $hours since the last save!"
-else
-    echo "OK: It has been $hours since the last save."
-fi
+    while :
+    do
+        result=$(redis_fetch $ip_addr INFO rdb_bgsave_in_progress)
+        if [[ $result == "0" ]]; then
+            echo "we're copying ${redis_dir}/${redis_dbname} to $target_dir"
+            exit 0
+        fi
+
+        sleep 1
+    done
+
+    #echo $redis_dbname
+    #echo $redis_dir
+}
+
+ip_addr=$1
+action=$2
+parameter=$3
+
+case $action in
+    FETCH|INFO)
+        redis_fetch $ip_addr INFO $parameter
+        ;;
+    BACKUP)
+        perform_backup $ip_addr $parameter
+        ;;
+    CONFIG)
+        redis_fetch $ip_addr CONFIG $parameter
+        ;;
+esac
