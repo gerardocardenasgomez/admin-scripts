3c0c97b56e21408af825dc90ab812e611844d3fa
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-08-25 11:57:27 -0400

    Add support for syslog idents

--- python/syslog-events/generator.py
+++ python/syslog-events/generator.py
@@ -13,6 +13,8 @@ events = [ "new_points", "new_user", "new_session", "get_reward" ]
 results = [ "successful", "failure", "else" ]
 devices = [ "android", "windows", "iphone", "blackberry" ]
 
+new_user_counter = 0
+
 f = open('datafile', 'w')
 
 while count < repeats:
@@ -20,9 +22,14 @@ while count < repeats:
     this_result = random.choice(results)
     this_device = random.choice(results)
 
+    syslog.openlog(this_event)
+
     message = uuid.uuid4().hex + " " + str(int(os.urandom(32).encode('hex'), 16)) + " " + this_event + " " + this_result + " " + this_device + "\n"
 
     count += 1
+
+    if this_event == "new_user":
+        new_user_counter += 1
 
     #print message
     sleep(0.0005)
@@ -32,5 +39,5 @@ while count < repeats:
     syslog.syslog(message)
     f.write(message)
 
-print "Messages published: {0}".format(count)
+print "Messages published: {0}, new_user published: {1}".format(count, new_user_counter)
 f.close()
