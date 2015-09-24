e21a9ac871f8466bc07b3666239a5b7c78cadd36
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-09-24 14:37:39 -0400

    Update parser.py file with comments

--- python/ip-hunter/parser.py
+++ python/ip-hunter/parser.py
@@ -4,9 +4,10 @@ import os
 import os.path
 import re
 
-#
-# ip counter
+# Dictionary with IP addresses as Keys and their counter as values
+# 
 ip_db = {}
+#
 
 # ip_addr matches valid IP addresses
 # full_uri matches something such as /wp-content/uploads/2015/06/file.png
@@ -27,6 +28,10 @@ document_uri = re.compile(document_uri_pattern)
 #
 #
 
+# parse_line accepts a string with a log event
+# parse_line should return at least an IP address
+# Ideally it should return IP address + resource 
+#
 def parse_line(line):
     result = ''
     
@@ -55,6 +60,9 @@ def parse_line(line):
     else:
         return result
 
+# Check that the arguments are all proper files and are accessible
+# os.access() would be enough except that directories will also return true for os.access()
+#
 for arg in sys.argv[1:]:
     if os.path.isfile(arg) and os.access(arg, os.R_OK):
         continue
@@ -62,6 +70,8 @@ for arg in sys.argv[1:]:
         print "File does not exist or unable to read: {0}".format(arg)
         exit(1)
 
+# For each file, parse through every line
+#
 for arg in sys.argv[1:]:
     file = open(arg)
     
@@ -74,8 +84,14 @@ for arg in sys.argv[1:]:
             if parsed_string is not None:
                 print arg + ' % ' + parsed_string
 
+# Make it easy to find the results portion by grepping for '--'
+# Make it visually clear that the following text is for results
+#
 print '-- + --' * 10
 
+# Print out the number of times each IP address was found in the file(s)
+# Items that appear only once are probably okay -- ignore those
+#
 for key,value in ip_db.items():
     if value > 1:
         print "{0} -- {1}".format(value, key)
