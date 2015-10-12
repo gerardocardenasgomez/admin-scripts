1355fcf198601af60bdd5fb86653dcc3df9777f3
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-10-12 16:20:22 -0500

    Add Ansible playbook for netstat intel output

--- ansible/netstat-overview.sh
+++ ansible/netstat-overview.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+echo "--ifconfig info--"
 ifconfig | egrep -o 'addr\:[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+echo "--Netstat info--"
 netstat -ptan | awk 'NR>2 {print $4" | "$5" | "$6" | "$7}' | sort | uniq -w 20
