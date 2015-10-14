8f3889defd25c5eba98d7f04cded9f8680439956
Gerardo Cardenas <gerardo@gerardobsd.com>
2015-10-14 11:25:36 -0500

    update to only print ip addresses

--- ansible/netstat-overview.sh
+++ ansible/netstat-overview.sh
@@ -2,5 +2,5 @@
 
 echo "--ifconfig info--"
 ifconfig | egrep -o 'addr\:[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | egrep -o '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
-echo "--Netstat info--"
-netstat -ptan | awk 'NR>2 {print $4" | "$5" | "$6" | "$7}' | sort | uniq -w 20
+#echo "--Netstat info--"
+#netstat -ptan | awk 'NR>2 {print $4" | "$5" | "$6" | "$7}' | sort | uniq -w 20
